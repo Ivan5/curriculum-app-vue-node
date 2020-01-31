0958ce7c2d7ed6af71d6c3e3a8fa2fcaa4811f4d
Ivan5 <converse_ivan@hotmail.com>
2020-01-31 17:00:45 -0600

    Add vuetify and make sass config

--- curriculum-front/src/router/index.js
+++ curriculum-front/src/router/index.js
@@ -1,27 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import DisplayCurriculum from "../views/DisplayCurriculum.vue";
+import UpsertCurriculum from "../views/UpsertCurriculum.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/curriculum",
+    name: "curriculum",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: DisplayCurriculum
+  },
+  {
+    path: "/curriculum/upsert",
+    name: "upsert",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: UpsertCurriculum
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
---
+++ curriculum-front/src/views/UpsertCurriculum.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Upsert Curriculum</h1>
+  </div>
+</template>
