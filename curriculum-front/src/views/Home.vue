a0ed26205f8e2686c4515bfc769196e80c73fe73
Ivan5 <converse_ivan@hotmail.com>
2020-02-05 14:59:21 -0600

    Make a layout of the card curriculum

--- curriculum-front/src/views/Home.vue
+++ curriculum-front/src/views/Home.vue
@@ -1,24 +1,17 @@
 <template>
-  <v-row no-gutters>
-    <v-col cols="4" xm="8">
-      <h1>All Curriculums</h1>
-      <v-btn>Create New</v-btn>
+  <v-row no-gutters class="home-page">
+    <v-col md="6" offset-md="3" sm="8" offset-sm="2">
+      <div class="page-header">
+        <h1>All Curriculums</h1>
+        <v-btn>Create New</v-btn>
+      </div>
 
-      <v-card class="mx-auto" max-width="344" outlined>
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="headline mb-1"
-              >Headline 5</v-list-item-title
-            >
-            <v-list-item-subtitle
-              >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis,
-              dolores quos similique alias at nisi non. Deserunt vitae animi at
-              quae dolorum natus pariatur consequatur aliquam. Odio nulla
-              dolorem non.</v-list-item-subtitle
-            >
-          </v-list-item-content>
-        </v-list-item>
-      </v-card>
+      <div class="curricula-list">
+        <v-card outlined>
+          <v-card-title class="headline">My Curriculum</v-card-title>
+          <v-card-subtitle>This is a description....</v-card-subtitle>
+        </v-card>
+      </div>
     </v-col>
   </v-row>
 </template>
