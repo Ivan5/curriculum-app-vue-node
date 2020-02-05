12d481634b7b4aec36e26529bc60c71e476ccb4c
Ivan5 <converse_ivan@hotmail.com>
2020-02-05 16:54:04 -0600

    finished coding pages from mockups

--- curriculum-front/src/views/DisplayCurriculum.vue
+++ curriculum-front/src/views/DisplayCurriculum.vue
@@ -1,5 +1,147 @@
 <template>
-  <div>
-    <h1>Display Curricula</h1>
-  </div>
+  <v-row no-gutters class="display-curriculum-page">
+    <v-col md="6" offset-md="3" sm="8" offset-sm="2">
+      <v-row>
+        <v-col>
+          <h1>Display Curriculum</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ipsum aut veritatis pariatur porro excepturi, vel nesciunt rem quas aliquid molestias quos corporis enim atque sit tempora ipsam ut doloremque.</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-expansion-panels multiple>
+            <v-expansion-panel>
+              <v-expansion-panel-header>Section 1</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-list subheader two-line flat>
+                  <v-subheader>Resources</v-subheader>
+
+                  <v-list-item-group v-model="settings" multiple>
+                    <v-list-item>
+                      <template v-slot:default="{ active, toggle }">
+                        <v-list-item-action>
+                          <v-checkbox v-model="active" color="primary" @click="toggle"></v-checkbox>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                          <v-list-item-title>Notifications</v-list-item-title>
+                          <v-list-item-subtitle>Allow notifications</v-list-item-subtitle>
+                        </v-list-item-content>
+                      </template>
+                    </v-list-item>
+
+                    <v-list-item>
+                      <template v-slot:default="{ active, toggle }">
+                        <v-list-item-action>
+                          <v-checkbox v-model="active" color="primary" @click="toggle"></v-checkbox>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                          <v-list-item-title>Sound</v-list-item-title>
+                          <v-list-item-subtitle>Hangouts message</v-list-item-subtitle>
+                        </v-list-item-content>
+                      </template>
+                    </v-list-item>
+
+                    <v-list-item>
+                      <template v-slot:default="{ active, toggle }">
+                        <v-list-item-action>
+                          <v-checkbox v-model="active" color="primary" @click="toggle"></v-checkbox>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                          <v-list-item-title>Video sounds</v-list-item-title>
+                          <v-list-item-subtitle>Hangouts video call</v-list-item-subtitle>
+                        </v-list-item-content>
+                      </template>
+                    </v-list-item>
+
+                    <v-list-item>
+                      <template v-slot:default="{ active, toggle }">
+                        <v-list-item-action>
+                          <v-checkbox v-model="active" color="primary" @click="toggle"></v-checkbox>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                          <v-list-item-title>Invites</v-list-item-title>
+                          <v-list-item-subtitle>Notify when receiving invites</v-list-item-subtitle>
+                        </v-list-item-content>
+                      </template>
+                    </v-list-item>
+                  </v-list-item-group>
+                </v-list>
+                <v-list subheader two-line flat>
+                  <v-subheader>Projects</v-subheader>
+
+                  <v-list-item-group v-model="settings" multiple>
+                    <v-list-item>
+                      <template v-slot:default="{ active, toggle }">
+                        <v-list-item-action>
+                          <v-checkbox v-model="active" color="primary" @click="toggle"></v-checkbox>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                          <v-list-item-title>Notifications</v-list-item-title>
+                          <v-list-item-subtitle>Allow notifications</v-list-item-subtitle>
+                        </v-list-item-content>
+                      </template>
+                    </v-list-item>
+
+                    <v-list-item>
+                      <template v-slot:default="{ active, toggle }">
+                        <v-list-item-action>
+                          <v-checkbox v-model="active" color="primary" @click="toggle"></v-checkbox>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                          <v-list-item-title>Sound</v-list-item-title>
+                          <v-list-item-subtitle>Hangouts message</v-list-item-subtitle>
+                        </v-list-item-content>
+                      </template>
+                    </v-list-item>
+
+                    <v-list-item>
+                      <template v-slot:default="{ active, toggle }">
+                        <v-list-item-action>
+                          <v-checkbox v-model="active" color="primary" @click="toggle"></v-checkbox>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                          <v-list-item-title>Video sounds</v-list-item-title>
+                          <v-list-item-subtitle>Hangouts video call</v-list-item-subtitle>
+                        </v-list-item-content>
+                      </template>
+                    </v-list-item>
+
+                    <v-list-item>
+                      <template v-slot:default="{ active, toggle }">
+                        <v-list-item-action>
+                          <v-checkbox v-model="active" color="primary" @click="toggle"></v-checkbox>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                          <v-list-item-title>Invites</v-list-item-title>
+                          <v-list-item-subtitle>Notify when receiving invites</v-list-item-subtitle>
+                        </v-list-item-content>
+                      </template>
+                    </v-list-item>
+                  </v-list-item-group>
+                </v-list>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+              <v-expansion-panel-header>Section 2</v-expansion-panel-header>
+              <v-expansion-panel-content>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa animi totam veritatis deleniti dolore voluptates eaque provident ut. Magnam totam eligendi hic neque quisquam vero in ducimus, laboriosam doloremque voluptate.</v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+              <v-expansion-panel-header>Section 3</v-expansion-panel-header>
+              <v-expansion-panel-content>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa animi totam veritatis deleniti dolore voluptates eaque provident ut. Magnam totam eligendi hic neque quisquam vero in ducimus, laboriosam doloremque voluptate.</v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
 </template>
