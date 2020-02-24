b80339254b38f3f9c1cf17abe2325a212012034c
Ivan5 <converse_ivan@hotmail.com>
2020-02-24 12:17:50 -0600

    Refactor timestamps in the schema

--- curriculum-back/db/Curriculum.js
+++ curriculum-back/db/Curriculum.js
@@ -1,30 +1,31 @@
 const mongoose = require("mongoose");
-const timestamps = require("mongoose-timestamp");
 
-const CurriculumSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const CurriculumSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    goal: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    sections: [
+      {
+        name: {
+          type: String,
+          required: true
+        },
+        resources: [String],
+        projects: [String]
+      }
+    ]
   },
-  goal: {
-    type: String
-  },
-  description: {
-    type: String
-  },
-  sections: [
-    {
-      name: {
-        type: String,
-        required: true
-      },
-      resources: [String],
-      projects: [String]
-    }
-  ]
-});
+  { timestamps: true }
+);
 
-CurriculumSchema.plugin(timestamps);
 const Curriculum = mongoose.model("Curriculum", CurriculumSchema);
 
 module.exports = Curriculum;
